4426644fae999362f80ee5a9ce2a9fefcb509cb0
NS <145736212+Ner-oV@users.noreply.github.com>
2025-05-03 03:55:58 -0400

    Update settings.py

--- jahfixmanager/settings.py
+++ jahfixmanager/settings.py
@@ -33,8 +33,8 @@ INSTALLED_APPS = [
 # Modelo personalizado de usuario
 AUTH_USER_MODEL = 'miApp.Empleado'
 
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 
 
 
